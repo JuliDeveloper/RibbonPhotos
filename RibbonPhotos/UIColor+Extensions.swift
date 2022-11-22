beca9b75919903a50d7f7e58d4889fc5492927e9
JuliaRomanenko <rja-97@yandex.ru>
2022-11-22 22:00:00 +0300

    SPRINT-8: Добавила кастомные цвета в проект

---
+++ RibbonPhotos/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 22.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "black")! }
+    static var ypBackground: UIColor { UIColor(named: "background")! }
+    static var ypWhite: UIColor { UIColor(named: "white")! }
+    static var ypWhiteLight: UIColor { UIColor(named: "whiteLight")! }
+    static var ypGray: UIColor { UIColor(named: "gray")! }
+    static var ypRed: UIColor { UIColor(named: "red")! }
+    static var ypBlue: UIColor { UIColor(named: "blue")! }
+}
