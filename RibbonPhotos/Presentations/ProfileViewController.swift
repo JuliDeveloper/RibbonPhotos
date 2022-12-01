cacebdc5a516d0a39ba272979b29cd4632200190
JuliaRomanenko <rja-97@yandex.ru>
2022-12-01 13:53:57 +0300

    SPRINT-9: Добавила ProfileViewController и настроила его внешний вид

---
+++ RibbonPhotos/Presentations/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 01.12.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
