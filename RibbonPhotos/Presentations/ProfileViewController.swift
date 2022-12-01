ab5cab6771848026ec548a372daea108df36c84d
JuliaRomanenko <rja-97@yandex.ru>
2022-12-01 14:44:56 +0300

    SPRINT-9: Добавила @IBOutlet'ы и @IBAction для ProfileViewController

--- RibbonPhotos/Presentations/ProfileViewController.swift
+++ RibbonPhotos/Presentations/ProfileViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var socialNetworkLabel: UILabel!
+    @IBOutlet private weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction private func logout(_ sender: UIButton) {
+    }
 }
