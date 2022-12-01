21e3c31d913363e6ff866331dd88dad93aa97e08
JuliaRomanenko <rja-97@yandex.ru>
2022-12-01 14:54:48 +0300

    SPRINT-9: Добавила SingleImageViewController и настроила его внешний вид

---
+++ RibbonPhotos/Presentations/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 01.12.2022.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+}
