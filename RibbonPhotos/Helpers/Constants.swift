e248d3c9f8f10409f71da1802cb143314d4db8b4
JuliaRomanenko <rja-97@yandex.ru>
2022-12-29 15:45:35 +0300

    SPRINT-10: Добавила файл Constants

---
+++ RibbonPhotos/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 29.12.2022.
+//
+
+import Foundation
+
+let AccessKey = "X1GaI9onDFAezERyuwGOfq4lezFJvZ7l78iW2YkJdyQ"
+let SecretKey = "MML8fojUnCLRTULAWzMWna99Ba8P38ZwUiv9qS178UM"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
