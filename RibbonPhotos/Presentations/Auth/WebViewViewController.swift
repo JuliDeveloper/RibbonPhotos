837d2ccc381c5c971286dd406c5f4f5532b7c759
JuliaRomanenko <rja-97@yandex.ru>
2023-01-06 16:21:48 +0300

    SPRINT-10: Добавла WebViewViewController; настроила переход с AuthViewController на WebViewViewController

---
+++ RibbonPhotos/Presentations/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 06.01.2023.
+//
+
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    private let identifier = "ShowWebView"
+}
