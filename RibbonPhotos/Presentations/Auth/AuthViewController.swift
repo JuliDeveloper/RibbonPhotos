7084ad032b67e6f9d12dc49d63c13234c48c0c93
JuliaRomanenko <rja-97@yandex.ru>
2023-01-06 16:12:54 +0300

    SPRINT-10: Создала AuthViewController; настроила его внешний вид; добавила для него Navigation Controller

---
+++ RibbonPhotos/Presentations/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  RibbonPhotos
+//
+//  Created by Julia Romanenko on 06.01.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
