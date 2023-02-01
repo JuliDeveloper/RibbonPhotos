88edee857c8d48246b151ac475f2e629ff12385f
JuliaRomanenko <rja-97@yandex.ru>
2023-02-01 15:38:56 +0300

    SPRINT-12: Изменила свойство dateFormmater на static, чтобы оно не создавалось каждый раз заново

--- RibbonPhotos/Helpers/Date + Extension.swift
+++ RibbonPhotos/Helpers/Date + Extension.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 
-private let dateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .long
-    formatter.timeStyle = .none
-    formatter.locale = Locale(identifier: "ru_ru")
-    return formatter
-}()
-
 extension Date {
-    var dateTimeString: String { dateFormatter.string(from: self) }
+    static private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_ru")
+        return formatter
+    }()
+    
+    var dateTimeString: String { Date.dateFormatter.string(from: self) }
     
     func convertStringToDate(_ string: String) -> Date? {
-        let dateFormatter =  ISO8601DateFormatter()
+        let dateFormatter = ISO8601DateFormatter()
         let date = dateFormatter.date(from: string)
         return date
     }
